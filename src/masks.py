1a0fc0f38098436c455ada7494d96edf648c7f89
Margo_Borodinowa <bmargaritaw@yandex.ru>
2025-12-20 01:27:16 +0300

    lost files

---
+++ src/masks.py
@@ -0,0 +1,31 @@
+def get_mask_card_number(numbers: str) -> str:
+    """Функция которая маскирует номер карты"""
+    code = []
+    for i in range(0, len(numbers), 4):
+        if numbers.isdigit() == False:
+            return "ошибка ввода"
+        elif len(numbers) != 16:
+            return "неверный номер"
+        elif len(numbers) == 16:
+            numi = numbers[i : i + 4]
+            code.append(numi)
+            mask = " ".join(code)
+            maske = f"{mask[0:7]}** **** {mask[-4:]}"
+        else:
+            return "введите номер"
+    return maske
+
+
+def get_mask_account(row: str) -> str:
+    """Функция которая маскирует номер счета"""
+    for i in range(0, len(row)):
+        if row.isdigit() == False:
+            return "ошибка ввода"
+        elif len(row) != 16:
+            return "неверный номер"
+        elif len(row) == 16:
+            numb = row[-4:]
+            mask = f"**{numb}"
+        else:
+            return "введите номер"
+    return mask
