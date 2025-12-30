97db6fd2ab9c0d4f2ad9112f33aee1451c3b27b2
Margo_Borodinowa <bmargaritaw@yandex.ru>
2025-12-30 20:10:46 +0300

    new_function addition

---
+++ src/processing.py
@@ -0,0 +1,19 @@
+def filter_by_state(roster: list[dict], status: str = "CANCELED") -> list[dict]:
+    """функция, которая фильтрует список по заданному значению и возвращает
+    список словарей, имеющих заданное значение"""
+
+    approved = []
+    for element in roster:
+        if status == element.get("state"):
+            approved.append(element)
+    return approved
+
+
+def sort_by_date(roster: list[dict], decreasing: bool = True) -> list[dict]:
+    """функция, сортирующая заданный список словарей по дате, в порядке убывания"""
+    if decreasing:
+        sorted_date: list[dict] = sorted(roster, key=lambda rost: rost.get("date"), reverse=True)
+        return sorted_date
+    else:
+        sorted_date: list[dict] = sorted(roster, key=lambda rost: rost.get("date"))
+        return sorted_date
