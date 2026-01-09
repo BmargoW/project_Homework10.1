c7c0d5195020474d8fd89b72d5fb275a95fcbb84
Margo_Borodinowa <bmargaritaw@yandex.ru>
2026-01-09 12:53:02 +0300

    fix changes

--- tests/test_widget.py
+++ tests/test_widget.py
@@ -1,5 +1,6 @@
 import pytest
-from src.widget import mask_account_card, get_date
+
+from src.widget import get_date, mask_account_card
 
 
 def test_mask_account_card() -> None:
@@ -8,7 +9,13 @@ def test_mask_account_card() -> None:
     assert mask_account_card("123456789876543") == "введите тип карты или 'счет'"
 
 
-@pytest.mark.parametrize('value, expected', [('2025-11-30T02:26:18.671407','30.11.2025'), ('2024-10-31T02:26:18.671407','31.10.2024'), ('2020-09-17T02:26:18.67','17.09.2020')])
-
-def test_get_date(value,expected):
+@pytest.mark.parametrize(
+    "value, expected",
+    [
+        ("2025-11-30T02:26:18.671407", "30.11.2025"),
+        ("2024-10-31T02:26:18.671407", "31.10.2024"),
+        ("2020-09-17T02:26:18.67", "17.09.2020"),
+    ],
+)
+def test_get_date(value:str, expected:str)-> None:
     assert get_date(value) == expected
