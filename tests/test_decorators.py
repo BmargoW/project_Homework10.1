84736eafee21e0b89cf7cf5c58229848c785bacd
Margo_Borodinowa <bmargaritaw@yandex.ru>
2026-01-30 11:32:23 +0300

    correct README.md

--- tests/test_decorators.py
+++ tests/test_decorators.py
@@ -1,5 +1,6 @@
 from src.decorators import log
 
+
 def test_log(capsys):
     log()
     captured = capsys.readouterr()
@@ -11,15 +12,18 @@ def test_log_my_function(capsys):
     @log()
     def my_function(x, y):
         return x / y
+
     my_function(1, 2)
     captured = capsys.readouterr()
 
     assert captured.out == "my_function ok\n"
 
+
 def test_log_my_function_error(capsys):
     @log()
     def my_function(x, y):
         return x / y
+
     my_function(1, 0)
     captured = capsys.readouterr()
 
