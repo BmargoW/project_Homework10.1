8804128f7fe12dc4e7bf26a95793f75b4be37cf3
Margo_Borodinowa <bmargaritaw@yandex.ru>
2026-02-09 14:31:22 +0300

    check tests

--- src/external_api.py
+++ src/external_api.py
@@ -1,45 +1,45 @@
-import requests
 import os
+
+import requests
 from dotenv import load_dotenv
+
 load_dotenv()
 
 t = {
     "id": 441945886,
     "state": "EXECUTED",
     "date": "2019-08-26T10:50:58.294041",
-    "operationAmount": {
-        "amount": "2000.00",
-        "currency": {
-            "name": "USD",
-            "code": "USD"
-        }
-    }
+    "operationAmount": {"amount": "2000.00", "currency": {"name": "USD", "code": "USD"}},
 }
 
+
 def report_operation(action):
-    if action["operationAmount"]["currency"]["code"] == 'RUB':
+    """функция, которая принимает на вход транзакцию и возвращает сумму транзакции (
+    amount) в рублях, тип данных — float. Если транзакция была в USD или EUR,
+    происходит обращение к внешнему API для получения
+    текущего курса валют и конвертации суммы операции в рубли."""
+    if action["operationAmount"]["currency"]["code"] == "RUB":
 
         return float(action["operationAmount"]["amount"])
     else:
         url = "https://api.apilayer.com/exchangerates_data/convert"
 
-        API_KEY = os.getenv('API_KEY')
+        API_KEY = os.getenv("API_KEY")
 
         payload = {
             "amount": (action["operationAmount"]["amount"]),
-            "from": (action["operationAmount"]["currency"]["name"]) ,
-            "to": "RUB"
+            "from": (action["operationAmount"]["currency"]["name"]),
+            "to": "RUB",
         }
-        headers = {
-            "apikey":  API_KEY
-        }
+        headers = {"apikey": API_KEY}
 
         response = requests.get(url, headers=headers, params=payload)
 
-        #status_code = response.status_code
+        # status_code = response.status_code
         resultet = dict(response.json())
         d = resultet["result"]
 
         return d
+
 
 print(report_operation(t))
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,6 +1,7 @@
-
 from unittest.mock import mock_open, patch
+
 from src.utils import uploading_content
+
 
 def test_uploading_content_valid_json():
     mock_data = '{"key": "value"}'
@@ -9,9 +10,10 @@ def test_uploading_content_valid_json():
         result = uploading_content("dummy.json")
         assert result == {"key": "value"}
 
+
 # Тест невалидного JSON
 def test_uploading_content_invalid_json():
-    mock_data = 'not a json'
+    mock_data = "not a json"
     m = mock_open(read_data=mock_data)
     with patch("builtins.open", m):
         result = uploading_content("dummy.json")
