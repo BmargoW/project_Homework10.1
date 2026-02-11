405cf65a0b4fd79983cb435640206b1ec434134e
Margo_Borodinowa <bmargaritaw@yandex.ru>
2026-02-12 01:45:11 +0300

    logging masks.py

--- src/masks.py
+++ src/masks.py
@@ -1,3 +1,13 @@
+import logging
+
+logger = logging.getLogger("masks")
+logger.setLevel(logging.DEBUG)
+file_handler = logging.FileHandler("../logs/masks.log", mode="w")
+file_formatter = logging.Formatter("%(asctime)s %(filename)s %(levelname)s: %(message)s")
+file_handler.setFormatter(file_formatter)
+logger.addHandler(file_handler)
+
+
 def get_mask_card_number(numbers: str) -> str:
     """Функция которая маскирует номер карты
     :rtype: str
@@ -5,14 +15,17 @@ def get_mask_card_number(numbers: str) -> str:
     code = []
     for i in range(0, len(numbers), 4):
         if numbers.isdigit() is False:
+            logger.error("There are more than just numbers in the card number")
             return "ошибка ввода"
         elif len(numbers) != 16:
+            logger.error("incorrect number of digits")
             return "неверный номер"
         elif len(numbers) == 16:
-            numi = numbers[i:i + 4]
+            numi = numbers[i : i + 4]
             code.append(numi)
             mask = " ".join(code)
             maske = f"{mask[0:7]}** **** {mask[-4:]}"
+            logger.info("the card number is being masked")
     return maske
 
 
@@ -20,10 +33,17 @@ def get_mask_account(row: str) -> str:
     """Функция которая маскирует номер счета"""
     for i in range(0, len(row)):
         if row.isdigit() is False:
+            logger.error("There are more than just numbers in the card number")
             return "ошибка ввода"
         elif len(row) != 16:
+            logger.error("incorrect number of digits")
             return "неверный номер"
         elif len(row) == 16:
             numb = row[-4:]
             mask = f"**{numb}"
+            logger.info("the account number is being masked")
     return mask
+
+
+if __name__ == "__main__":
+    get_mask_card_number("1234567812345679")
