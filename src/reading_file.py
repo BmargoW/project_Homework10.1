16e4d79042e41db358c10cd2cdff2904a45d6c81
Margo_Borodinowa <bmargaritaw@yandex.ru>
2026-02-22 01:26:01 +0300

    correction reading_file.py

--- src/reading_file.py
+++ src/reading_file.py
@@ -4,6 +4,8 @@ import csv
 
 
 def reading_files_csv(name_file):
+    """функция, которая считывает финансовые операции из файла-CSV
+    и возвращает список словарей с транзакциями"""
     with open(name_file) as file:
         transaction = csv.DictReader(file)
         rows = []
@@ -16,6 +18,8 @@ print(reading_files_csv("../transactions.csv"))
 
 
 def reading_files_exel(name_f):
+    """Функция, которая считывает  финансовые операции из файла - Excel
+    и возвращает список словарей с транзакциями"""
     excel_data = pd.read_excel(name_f)
     res = excel_data.to_dict(orient="records")
 
