6c636d19088fb03b1fcbd62cca65e81d602047b2
Margo_Borodinowa <bmargaritaw@yandex.ru>
2026-02-22 01:08:37 +0300

    correction reading_file

--- src/reading_file.py
+++ src/reading_file.py
@@ -2,21 +2,24 @@ import pandas as pd
 
 import csv
 
+
 def reading_files_csv(name_file):
-    with open (name_file) as file:
+    with open(name_file) as file:
         transaction = csv.DictReader(file)
         rows = []
         for row in transaction:
             rows.append(row)
         return rows
 
+
 print(reading_files_csv("../transactions.csv"))
 
 
 def reading_files_exel(name_f):
-      excel_data = pd.read_excel(name_f)
-      res = excel_data.to_dict(orient='records')
+    excel_data = pd.read_excel(name_f)
+    res = excel_data.to_dict(orient="records")
 
-      return res
+    return res
+
 
 print(reading_files_exel("../transactions_excel.xlsx"))
